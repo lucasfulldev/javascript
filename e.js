b7da1317092651d8eca5e99e7a5caaf698e4752d
lucaslindo <lukasantosilva@yahoo.com.br>
2017-04-15 16:34:01 -0300

    Add files via upload

---
+++ e.js
@@ -0,0 +1,18 @@
+﻿function evento_entrar(){
+
+alert("Seja Bem Vindo(a) Mestre");
+
+}
+function evento_sair()
+
+{
+
+alert("Até a Proxima Visitar Mestre");
+
+}
+function evento_clique()
+
+{
+ window.document.getElementById('id2').style.color='red';
+
+}
